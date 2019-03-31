2e6555248b49ad1d9ad9df59ec16e082a4d654d7
Thanh Hoai <hoaintt97.gtvt@gmail.com>
2019-03-31 11:10:14 +0700

    Create migrate

---
+++ database/migrations/2019_03_30_064240_create_nhanvien_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNhanvienTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nhanvien', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('ho_ten');
+            $table->string('chuc_vu');
+            $table->string('email')->unique();
+            $table->string('sdt')->unique();
+            $table->text('mo_ta');
+            $table->text('hinh_anh');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nhanvien');
+    }
+}
---
+++ database/migrations/2019_03_30_074323_create_baocaocsvc_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBaocaocsvcTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('baocaocsvc', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('ma_sv_utc');
+            $table->foreign('ma_sv_utc')
+                ->references('id')->on('sinhvienutc')
+                ->onDelete('cascade');
+            $table->unsignedBigInteger('ma_phong_csvc');
+            $table->foreign('ma_phong_csvc')
+                ->references('id')->on('phong_csvc')
+                ->onDelete('cascade');
+            $table->tinyInteger('tinh_trang');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('baocaocsvc');
+    }
+}
