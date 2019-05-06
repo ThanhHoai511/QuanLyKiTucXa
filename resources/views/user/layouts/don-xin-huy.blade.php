55ed444b298a17951f3b2cd1b8bab56bd4b00016
hoaintt <hoaintt@hblab.vn>
2019-05-06 19:13:50 +0700

    hoai

--- resources/views/user/layouts/don-xin-huy.blade.php
+++ resources/views/user/layouts/don-xin-huy.blade.php
@@ -9,8 +9,20 @@
     <div class="container col-md-8" style="height: 550px;">
         @include('admin.layouts.flash-msg')
         <h3 style="text-align: center; margin:20px;">Đơn xin chấm dứt hợp đồng</h3>
-        <form class="col-md-12" enctype="multipart/form-data" method="post" id="form">
+        <form class="col-md-12" method="post" id="form">
             {{ csrf_field() }}
+            <div class="row">
+                <div class="form-group col-md-6">
+                    <label for="ma_sv">Mã sinh viên</label>
+                    <input type="text" name="ma_sv" class="form-control" id="ma_sv" placeholder="Nhập mã sinh viên">
+                    <span class="error" id="errMSV"></span>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="ho_ten">Họ tên</label>
+                    <input type="text" name="ho_ten" class="form-control" id="ho_ten" placeholder="Nhập họ tên">
+                    <span class="error" id="errHoTen"></span>
+                </div>
+            </div>
             <div class="row">
                 <div class="form-group col-md-6">
                     <label for="khu_nha">Chọn khu nhà</label>
@@ -29,7 +41,7 @@
                     <span class="error" id="errPhong"></span>
                 </div>
             </div>
-            <div class="form-group">
+            <div class="form-group col-md-12" style="margin: 10px 500px;">
                 <button class="btn btn-primary" type="submit">Gửi đơn</button>
                 <a href="{{ route('home') }}"><button class="btn btn-danger" type="submit">Hủy</button></a>
             </div>
@@ -39,6 +51,57 @@
         <option value=""></option>
     </div>
     <script>
+        $('#form').submit(function (e) {
+            e.preventDefault();
+            if ($('#ma_sv').val() == '') {
+                $('#errMSV').text('Bạn phải nhập mã sinh viên!');
+                $('#ma_sv').focus();
+                return false;
+            }
+            if ($('#ho_ten').val() == '') {
+                $('#errHoTen').text('Bạn phải nhập họ tên!');
+                $('#ho_ten').focus();
+                return false;
+            }
+            if ($('#khu_nha').val() == '') {
+                $('#errKhuNha').text('Bạn phải chọn khu nhà!');
+                $('#khu_nha').focus();
+                return false;
+            }
+            if ($('#ma_phong').val() == '') {
+                $('#errPhong').text('Bạn phải chọn phòng đang ở!');
+                $('#ma_phong').focus();
+                return false;
+            }
+        });
+        $('#ma_sv').keyup(function () {
+            if ($('#ma_sv').val() == '') {
+                $('#errMSV').text('Bạn phải nhập mã sinh viên!');
+            } else {
+                $('#errMSV').text('');
+            }
+        });
+        $('#ho_ten').keyup(function () {
+            if ($('#ho_ten').val() == '') {
+                $('#errHoTen').text('Bạn phải nhập họ tên!');
+            } else {
+                $('#errHoTen').text('');
+            }
+        });
+        $('#khu_nha').change(function () {
+            if ($('#khu_nha').val() == '') {
+                $('#errKhuNha').text('Bạn phải chọn khu nhà!');
+            } else {
+                $('#errKhuNha').text('');
+            }
+        });
+        $('#ma_phong').change(function () {
+            if ($('#ma_phong').val() == '') {
+                $('#errPhong').text('Bạn phải chọn phòng đang ở!');
+            } else {
+                $('#errPhong').text('');
+            }
+        });
         $("#khu_nha").change(function() {
             var ma_khu = $(this).val();
             $.ajax({
